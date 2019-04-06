dd493564bf35a0a6115287334600983590127e7d
Frederik van Veen <frederikvanveen@wcw-stud-145-109-2-52.wireless.uva.nl>
2019-04-06 14:00:53 +0200

    added code

---
+++ spacex.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+class Rocket():
+    def __init__(self, spacecraft, nation, payload_mass, payload_volume, mass, base_cost, fuel_to_weight):
+        """
+        Initialize a Room
+        give it an id, name, description, directions, with associated connections, visits and conditions
+        """
+        self.spacecraft = spacecraft
+        self.nation = nation
+        self.payload_mass = payload_mass
+        self.payload_volume = payload_volume
+        self.mass = mass
+        self.base_cost = base_cost
+        self.fuel_to_weight = fuel_to_weight
+
+    def __str__(self):
+        return self.spacecraft + " " + str(self.mass)
+
+class Item():
+    def __init__(self, spacecraft, nation, payload_mass, payload_volume, mass, base_cost, fuel_to_weight):
+        """
+        Initialize a Room
+        give it an id, name, description, directions, with associated connections, visits and conditions
+        """
+        self.parcel_ID = parcel_ID
+        self.mass = mass
+        self.volume = volume
+
+    def __str__(self):
+        return self.spacecraft + " " + str(self.mass)
+
+def ReadRockets(INPUT_CSV):
+    rockets = []
+    df = pd.read_csv(INPUT_CSV)
+    for index, row in df.iterrows():
+        rocket = Rocket(row["Spacecraft"], row["Nation"], row['Payload Mass (kgs)'], row['Payload Volume (m3)'],row['Mass (kgs)'], row['Base Cost($)'], row['Fuel-to-Weight'])
+        rockets.append(rocket)
+    return rockets
+
+"parcel_ID","mass (kg)","volume (m^3)"
+
+def ReadCargo(INPUT_CSV):
+    cargolist = []
+    df = pd.read_csv(INPUT_CSV)
+    for index, row in df.iterrows():
+        item = Item(row['parcel_ID'], row['mass (kg)'], row['volume (m^3)'])
+        cargolist.append(item)
+    return cargolist
+
+
+
+if __name__ == "__main__":
+    rockets = ReadRockets('rockets.csv')
+    for rocket in rockets:
+        print(rocket)
